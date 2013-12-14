7b42bc21acde495eeeac0827fab319169270ecd3
John Wible <john.wible@gmail.com>
2013-12-14 13:38:32 -0500

    Update Problem 5.py

--- Problem 5.py
+++ Problem 5.py
@@ -1,3 +1,5 @@
+"""What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"""
+
 def Divisible_1_to_20 (x):
     for i in range (1,21,1):
         if x % i > 0:
