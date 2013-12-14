1e0ca2e1f685044d716907eca282ccecba9e66c4
John Wible <john.wible@gmail.com>
2013-12-14 13:44:51 -0500

    Update Problem 6.py

--- Problem 6.py
+++ Problem 6.py
@@ -1,8 +1,10 @@
+"""Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum."""
+
 sum_of_squares = 0
 square_of_sums = 0
 
 for i in range (101):
-    sum_of_squares = sum_of_squares + i*i
-    square_of_sums = square_of_sums + i
+    sum_of_squares += i*i
+    square_of_sums += i
 answer = (square_of_sums*square_of_sums - sum_of_squares)
 print (answer)
